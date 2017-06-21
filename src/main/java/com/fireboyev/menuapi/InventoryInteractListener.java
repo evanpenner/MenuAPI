d71880bd2619ec2ab19d7e4c562e12d038df9845
SirWill <brieftaubenman@gmail.com>
2017-06-21 19:41:09 +0200

    Count clicks to prevent fast clicks caused by inventory tweaks

--- src/main/java/com/fireboyev/menuapi/InventoryInteractListener.java
+++ src/main/java/com/fireboyev/menuapi/InventoryInteractListener.java
@@ -11,6 +11,7 @@ import org.spongepowered.api.event.item.inventory.DropItemEvent;
 import org.spongepowered.api.event.item.inventory.InteractInventoryEvent;
 import org.spongepowered.api.item.inventory.Container;
 import org.spongepowered.api.item.inventory.property.SlotIndex;
+import org.spongepowered.api.scheduler.Task;
 import org.spongepowered.api.util.Tristate;
 
 import com.fireboyev.menuapi.objects.Button;
@@ -23,20 +24,38 @@ public class InventoryInteractListener {
 		MenuAPI.removeViewer(player);
 	}
 
+	private int clicks = 0;
 	@Listener(order = Order.EARLY)
 	public void onClick(ClickInventoryEvent event, @First Player player,
 			@Getter("getTargetInventory") Container container) {
 		if (isInInventoryMenu(player)) {
 			if (!event.getTargetInventory().equals(player.getInventory())) {
-				Menu menu = MenuAPI.getViewingMenu(player);
-				SlotIndex slotPos = event.getTransactions().get(0).getSlot().getProperty(SlotIndex.class, "slotindex")
-						.get();
-				Button button = menu.getButtonByPos(slotPos);
-				// System.out.println("SLOT POSITION: " + slotPos.getValue());
-				ButtonExecutor exe = button.getExecutor();
-				if (exe != null) {
-					exe.onButtonClick(event, menu, button, player);
-				}
+				// Count clicks to prevent fast clicks caused by inventory tweaks
+				clicks++;
+				Task.builder().execute(new Runnable() {
+					@Override
+					public void run() {
+						if (clicks < 2) {
+							Menu menu = MenuAPI.getViewingMenu(player);
+							SlotIndex slotPos = event.getTransactions().get(0).getSlot().getProperty(SlotIndex.class, "slotindex")
+									.get();
+							Button button = menu.getButtonByPos(slotPos);
+							// System.out.println("SLOT POSITION: " + slotPos.getValue());
+							ButtonExecutor exe = button.getExecutor();
+							if (exe != null) {
+								exe.onButtonClick(event, menu, button, player);
+							}
+						}
+					}
+				}).delayTicks(1).submit(MenuAPI.getInstance());
+
+				// Reset click count
+				Task.builder().execute(new Runnable() {
+					@Override
+					public void run() {
+						clicks = 0;
+					}
+				}).delayTicks(2).submit(MenuAPI.getInstance());
 			}
 		}
 	}
